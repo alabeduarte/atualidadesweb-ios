38cf284cfeaf919b729dab7814d03eff0e084b44
Alabê Duarte <alabeduarte@gmail.com>
2013-08-29 18:41:56 -0300

    setting up kiwi for unit tests

---
+++ AtualidadesWebTests/FetchNewsSpec.h
@@ -0,0 +1,13 @@
+//
+//  FetchNewsSpec.h
+//  AtualidadesWeb
+//
+//  Created by Alabê Duarte on 8/29/13.
+//  Copyright (c) 2013 atualidadesweb. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FetchNewsSpec : SenTestCase
+
+@end
